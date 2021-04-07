4a897694029a82b5d1ee2b0583151e7dccb948b1
Nicolas <n.maes@codeur-online>
2021-04-07 15:58:13 +0200

    2eme ajout avant fin de journée

--- contact_us.php
+++ contact_us.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="Présentation des métiers du web" content="Site internet regroupant les articles concernant les métiers du web, rédigés par la promo 68 de l'Access Code School">
+    <meta name="Page de contact" content="page web contenant nos ">
     <link rel="stylesheet" href="style.css">
  <link rel="shortcut icon" href="source" type="image/x-icon">
-    <title>Landing page</title>
+    <title>Page de contact</title>
 </head>
 <body>
 
