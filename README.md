bdaf8d934f2917834a306e2fa8483ca6d3bf9c3b
Nicolas <n.maes@codeur-online>
2021-04-07 15:48:21 +0200

    ajout des fichiers principaux et des illustrations

--- README.md
+++
@@ -1,4 +0,0 @@
-# luc-nicolas
-
-## Ce repository représente le travail effectué en binôme à partir du 6 avril 2021.
-
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Présentation des métiers du web" content="Site internet regroupant les articles concernant les métiers du web, rédigés par la promo 68 de l'Access Code School">
+    <link rel="stylesheet" href="style.css">
+ <link rel="shortcut icon" href="source" type="image/x-icon">
+    <title>Landing page</title>
+</head>
+<body>
