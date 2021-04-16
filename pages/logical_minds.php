48595c3c4dc85b6dc762fc31204a555b1437ca17
LucLeneuf <luc.leneuf@hotmail.fr>
2021-04-16 15:39:21 +0200

    maj

--- pages/logical_minds.php
+++ pages/logical_minds.php
@@ -35,15 +35,84 @@
 
 
 
-    <div class="container_article font_text">
-        <div>
+    <div class="container_gallery font_text">
 
-        <a href="nicolas.php">nicolas</a>
-        <a href="nathan.php">nathan</a>
-        <a href="gael.php">gael</a>
-        <a href="paule.php">paule</a>
-        <a href="adrien.php">adrien</a>
-        <a href="williams.php">williams</a>
+    <a href="adrien.php">
+        <div class="container_gallery__item">
+            <p>
+                <span class="title"> La cryptographie</span> <br><br>
+
+                <span class="lightgrey">
+                La cryptographie regroupe les disciplines ayant pour but de rechercher des moyens d’assurer la protection de données
+                </span> 
+            </p>
+        </div>
+    </a>
+
+       
+        <a href="nicolas.php">
+            <div class="container_gallery__item">
+                <p>
+                    <span class="title"> Les langages </span> <br><br>
+
+                    <span class="lightgrey">
+                    Quand on s’intéresse de près au développement informatique. Pas facile de s’y retrouver dans toutes ces technologies; leurs rôles, leurs atouts, leurs champs d’application…
+                    </span> 
+
+                </p>
+            </div>
+        </a>
+
+
+        <a href="nathan.php">
+            <div class="container_gallery__item">
+                <p>
+                    <span class="title"> Le chef de projet </span> <br><br>
+
+                    <span class="lightgrey">
+                    Le chef de projet a pour mission principale de prendre en main le cahier des charges d’un projet et de le mettre en œuvre en analysant les besoins, réalisant des reportings.
+                    </span> 
+                </p>
+            </div>
+        </a>
+
+
+        <a href="williams.php">
+            <div class="container_gallery__item">
+                <p>
+                    <span class="title"> Le Pentesteur</span> <br><br>
+
+                    <span class="lightgrey">
+                    Le métier de « Pentesteur » peut aussi être assimilé à du « Ethical hacking » ou Piratage éthique en français car il utilise les mêmes méthodes que les autres hackers mais sans arrières pensées malveillante.
+                    </span> 
+                </p>
+            </div>
+        </a>
+
+
+        <a href="gael.php">
+            <div class="container_gallery__item">
+                <p>
+                    <span class="title"> L'analyste  </span> <br><br>
+                    
+                    <span class="lightgrey">
+                    L’analyste test et validation, aussi appelé ingénieur test ou testeur, a pour activité principale la validation des logiciels ou applications avant leur déploiement.
+                    </span> 
+                </p>
+            </div>
+        </a>
+
+        <a href="paule.php">
+            <div class="container_gallery__item">
+                <p>
+                    <span class="title"> Le directeur Commercial </span> <br><br>
+                    
+                    <span class="lightgrey">
+                    Un site internet est certainement la plus belle vitrine qu’une entreprise ait besoin. Pour la valoriser en terme de communication web, il faut penser à son intérêt.
+                    </span> 
+                </p>
+            </div>
+        </a>
     </div>  
 
 </div>
